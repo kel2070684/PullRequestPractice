ed254a25c4ea0475c9ef14442a23f9988c9edf2a
Kelly Sturdevant <kel2070684@maricopa.edu>
2025-04-05 12:44:38 -0700

    adding s25_Sturdevant.md & s25_Sturdevant.py

---
+++ s25_Sturdevant.md
@@ -0,0 +1,1 @@
+"Authority should derive from the consent of the governed, not from the threat of force!" - Barbie, Toy Story 3 (2010)
---
+++ s25_Sturdevant.py
@@ -0,0 +1,10 @@
+"""
+Kelly Sturdevant
+Module 08 Programming Project
+
+This program prints out a movie quote from Toy Story 3 (2010)
+"""
+
+print(
+    "\n'Authority should derive from the consent of the governed, not from the threat of force!' - Barbie, Toy Story 3 (2010)\n"
+)
